7f18058fc99d2df2a464dec92b00a058697f00e0
lengyangyu <2237616014@qq.com>
2021-09-28 16:30:45 +0800

    all

--- operator_new_delete/reload_new.cpp
+++ operator_new_delete/reload_new.cpp
@@ -20,5 +20,7 @@ int main(){
     // char *p = new char('a');
     char *p = new(__FILE__,__LINE__) char;    //重载的 new 可以直接带参数使用的，只要重载了相对应的形式就可以，其他的不用管
     ::operator delete(p,__FILE__,__LINE__);    //重载的delete需要加 operator 前缀才可以使用，而且格式必须如左所示，在这里 :: 可以不要 ，仅代表全局的意思
+    int *a = new int[10];
+    delete [] a;// ::operator delete[](a);
     return 0;
 }
