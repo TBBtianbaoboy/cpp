e8d90a53bdab55e752232800800ef04eecf7c584
lengyangyu <2237616014@qq.com>
2021-10-19 10:32:19 +0800

    all

--- memory_management/12/12.cpp
+++ memory_management/12/12.cpp
@@ -14,6 +14,7 @@ class B{
 
 
 //@desc 继承自空类占 4 个字节
+//@why 空白基类最优化，优化为 0 字节
 class C:public A{
     private:
         int a;
@@ -21,9 +22,19 @@ class C:public A{
         C():a(1){}
 };
 
+//@desc 基类private数据成员也会继承下来，只是不能访问
+//@desc 基类虚函数表指针也会继承下来
+class D:public C,public B{
+    private:
+        int a;
+    public:
+        D():a(2){
+        }
+};
+
 
 int main(){
     dbg(sizeof(A),sizeof(B));
-    dbg(sizeof(C));
+    dbg(sizeof(C),sizeof(D));
     return 0;
 }
